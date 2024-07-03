7f34a7a5e6f869adf7a49599f59a63109f98b2da
sidhant76 <150133951+sidhant76@users.noreply.github.com>
2024-07-03 08:56:13 +0530

    spring file

---
+++ Laptop.java
@@ -0,0 +1,15 @@
+package com.example.MyApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+
+public class Laptop {
+	
+	public void compile() {
+		
+		System.out.println("Laptop class work properly");
+	}
+
+}
